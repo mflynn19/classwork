5b6c2314bbaa194e37ed4a800f25440dd5fc50ce
Meghan Flynn <meghan.flynn@theflynn.org>
2018-09-18 14:51:08 -0400

    help me

---
+++ src/basicIO91818.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class basicIO91818{
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	String text = in.nextLine();
+	System.out.println(text);
+	in.close();
+}
+}
